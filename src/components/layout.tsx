d309b9f0d741ccef30537c6443120865bbbadd6c
dcalsky <dcalsky@163.com>
2020-12-03 23:31:44 +0800

    feat: responsive layout and logout menu

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,65 +2,111 @@ import {
   UserOutlined,
   LaptopOutlined,
   NotificationOutlined,
+  DownloadOutlined,
+  LoginOutlined, GithubOutlined
 } from "@ant-design/icons"
-import { Menu, Breadcrumb, Layout as ALayout } from "antd"
-import { Header, Content } from "antd/lib/layout/layout"
+import { Menu, Breadcrumb, Layout as ALayout, Button, Dropdown, Tooltip } from "antd"
+import { Header, Content, Footer } from "antd/lib/layout/layout"
 import Sider from "antd/lib/layout/Sider"
 import SubMenu from "antd/lib/menu/SubMenu"
 import { Link, PageProps } from "gatsby"
-import React from "react"
+import { observer } from "mobx-react-lite"
+import React, { useState } from "react"
+import { userStore } from "../stores"
+import "./layout.less"
 
-export default function Layout({ children, path }: PageProps) {
+const AccountMenu = observer(() => {
+  const onLogout = () => {
+    userStore.logout()
+  }
   return (
-    <ALayout>
-      <Header className="header">
-        <div className="logo" />
-      </Header>
-      <ALayout>
-        <Sider className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={[path]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100%", borderRight: 0 }}
-          >
-            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-              <Menu.Item key="/">
-                <Link to="/">Index Page</Link>
-              </Menu.Item>
-              <Menu.Item key="/list/">
-                <Link to="/list/">List Page</Link>
-              </Menu.Item>
-              <Menu.Item key="/clear/">
-                <Link to="/clear">Clear Page</Link>
-              </Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <ALayout style={{ padding: "0 24px 24px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
+    <Menu>
+      <Menu.Item onClick={onLogout} icon={<LoginOutlined />}>
+        Logout
+      </Menu.Item>
+    </Menu>
+  )
+})
+
+export default observer(({ children }) => {
+  // const [collapsed, setCollapsed] = useState(false)
+  // const onCollapse = (collapsed: boolean) => {
+  //   setCollapsed(collapsed)
+  // }
+  const user = userStore.user
+  if (!user) {
+    return null
+  }
+  return (
+    <ALayout style={{ minHeight: "100vh" }}>
+      <Sider
+        // collapsible
+        breakpoint="lg"
+        collapsedWidth="0"
+        // onCollapse={onCollapse}
+        className="site-layout-background"
+      >
+        <Menu
+          mode="inline"
+          theme="dark"
+          defaultSelectedKeys={["/"]}
+          defaultOpenKeys={["sub1"]}
+          style={{ height: "100%", borderRight: 0 }}
+        >
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+            <Menu.Item key="/">
+              <Link to="/">Index Page</Link>
+            </Menu.Item>
+            <Menu.Item key="/list/">
+              <Link to="/list/">List Page</Link>
+            </Menu.Item>
+            <Menu.Item key="/clear/">
+              <Link to="/clear">Clear Page</Link>
+            </Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <ALayout className="site-layout">
+        <Header className="site-layout-sub-header-background">
+          <div style={{ flex: "1 1 0" }} />
+          <div className="header-menus">
+            <Tooltip className="header-menu" placement="bottom" title={"Github"}>
+              <Button href="https://github.com/dcalsky/gatsby-mobx-typescript" type="text" icon={<GithubOutlined />} />
+            </Tooltip>
+            <Dropdown
+              className="header-menu"
+              overlay={<AccountMenu />}
+              placement="bottomCenter"
+            >
+              <Button icon={<UserOutlined />} type="text">
+                {user.name}
+              </Button>
+            </Dropdown>
+          </div>
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 768
+          }}
+        >
+          {/* <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 768,
-            }}
-          >
-            {children}
-          </Content>
-        </ALayout>
+          </Breadcrumb> */}
+          {children}
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Footer</Footer>
       </ALayout>
     </ALayout>
   )
-}
+})
